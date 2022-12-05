bbb3afb030999b68cd2a6e057fada590bd6be8f9
lizimu1 <2879179514@qq.com>
2022-12-05 18:39:39 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { App } from './App2'
+import './style.css'
+import { Bar } from './views/Bar'
+import { Foo } from './views/Foo'
+import {createRouter,createWebHashHistory} from 'vue-router'
+
+const routes = [
+    { path: '/', component: Foo },
+    { path: '/about', component: Bar },
+  ]
+
+  const router = createRouter({
+    
+    history: createWebHashHistory(),
+    routes, 
+  })
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
